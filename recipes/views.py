46dcc9f2dafa5acafcfa89047a7da413796a152a
Pedro Lins de Souza <pedrols.oc@gmail.com>
2023-01-16 17:33:28 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -12,9 +11,3 @@ def home(request):
     })
 
 
-def contato(request):
-    return HttpResponse('contato')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
