8f14f46c096d19035565774ce8067ee63851645f
Pedro Lins de Souza <pedrols.oc@gmail.com>
2023-01-16 17:29:30 -0300

    Seção 5 Aula 26 Templates, paths e namespaces

---
+++ recipes/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    # return render(request, 'recipes/home.html')
+    # return render(request, 'recipes/home.html', status=201)
+    return render(request, 'recipes/home.html', context={
+        'name': 'Pedro Lins de Souza',
+    })
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
