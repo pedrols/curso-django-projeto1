cc6fc1700464350f971711314ba1f212e04dfb81
Pedro Lins de Souza <pedrols.oc@gmail.com>
2023-01-23 01:57:27 -0300

    Dados preenchidos com faker e django (for, etc)

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 # Create your views here.
 
 
 def home(request):
-    # return render(request, 'recipes/home.html')
-    # return render(request, 'recipes/home.html', status=201)
+    # usando list comprehension para preencher multiplas receitas
+    # falta implementar no html ainda
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Pedro Lins de Souza',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
@@ -15,5 +17,6 @@ def recipe(request, id):
     # return render(request, 'recipes/home.html')
     # return render(request, 'recipes/home.html', status=201)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Pedro Lins de Souza',
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
